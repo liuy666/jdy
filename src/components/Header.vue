1003b54e15698e55a52557fe80293baba7aa7aa2
liuy666 <472961306@qq.com>
2018-04-23 21:56:48 +0800

    2018.4.23

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+<header>
+    <div class="header-left">
+        <span class="logo router-link-active"><img src="" /></span> 
+        <span id="header-menu" class="menu"><img src="" /></span>
+    </div> 
+    <div class="header-bottom">
+        <div class="bottom-tech">
+            <div id="header-tech" class="clearfix tech-title">
+                <span class="name">技术</span>
+                <span class="arrow"></span>
+            </div> 
+            <div class="sub-menu">
+                <p class="sub-title"> 人脸识别 </p> 
+                <p class="sub-title router-link-exact-active router-link-active"> 行人检测 </p>
+            </div>
+        </div> 
+        <ul class="bottom-list">
+            <li class="sub-title"> 产品 </li> 
+            <li class="sub-title"> 关于我们 </li> 
+            <li class="sub-title"> 加入我们 </li>
+        </ul>
+    </div>
+</header>
+</template>
+<script>
+import './Hearder.scss'
+export default{
+
+}
+</script>
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './sass/base.scss'
+
+(function (doc, win) {
+    var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) return;
+        docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
+    };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
 
 Vue.config.productionTip = false
 
